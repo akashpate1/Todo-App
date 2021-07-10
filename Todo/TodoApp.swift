f98550f1470b55cf4ef277431b86e167b7f8f34c
Akash Pate <pateakash79@gmail.com>
2021-07-10 07:39:04 +0530

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Akash Pate on 10/07/21.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
